60ce20c2b88455e816d3e971482041d35a0acf65
thomalex <thomas.gae@posteo.de>
2021-01-20 16:58:42 +0100

    Yeah, got an array with 10 links

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 const getWebsite = async (_) => {
   try {
@@ -7,7 +9,19 @@ const getWebsite = async (_) => {
       'https://memegen-link-examples-upleveled.netlify.app/',
     );
     const parser = await data.text();
-    console.log(parser);
+    const dom = await new JSDOM(parser);
+    const window = await dom.window.document;
+    const imgNodeList = await window.querySelectorAll('img');
+    let imgArr = [];
+    for (let elem of imgNodeList.values()) {
+      imgArr.push(elem.src);
+    }
+    let arr10 = imgArr.slice(0, 9);
+    // const parent = await dom.window.document.querySelector('section');
+    // const children = await parent.childNodes;
+    // const arr = await [...children];
+
+    console.log(arr10);
   } catch (error) {
     console.log(error);
   }
